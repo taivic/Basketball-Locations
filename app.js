0442efcddf56e757d8280ff33de28468f62d7914
taivic <vicky.yen.tai@gmail.com>
2016-10-17 09:29:01 -0500

    new input

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("#submit").submit(function(e){
 		e.preventDefault();
 		var type = $("#type").val();
-	    var level = $("#lastName").val();
+	    var level = $("#level").val();
+	    var zipcode = $("#zipcode").val();
 	})
 })
