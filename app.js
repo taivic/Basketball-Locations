e499c542f7cdfdff512460bb668af4a4ec68d294
Patrick Ford <patrickford@msn.com>
2016-10-17 08:07:58 -0700

    Fixed two column layout of map and form

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
-	$("#submit").submit(function(e){
-		e.preventDefault();
-		var type = $("#type").val();
-	    var level = $("#level").val();
-	    var zipcode = $("#zipcode").val();
-	})
-})
+
+  $("#submit").submit(function(e){
+    e.preventDefault();
+    var type = $("#type").val();
+    var level = $("#level").val();
+    var zipcode = $("#zipcode").val();
+  });
+});
